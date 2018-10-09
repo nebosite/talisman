275cf7b65a1d33fdfbf5678a4d7dd6ed90dbf810
Eric Jorgensen <erjorge@microsoft.com>
2018-10-09 14:02:54 -0700

    Initial App

---
+++ src/WindowsClient/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Talisman
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+
+        }
+
+        int frame = 0;
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            frame++;
+            if (frame % 3 == 0)
+            {
+                this.Left -= 2;
+                this.Top += 1;
+
+            }
+        }
+    }
+}
