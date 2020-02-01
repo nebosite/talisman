5be7a77e8778a25c659ad621c15c1e30d9f8c82d
nebosite <ericjorg@thejcrew.net>
2020-01-31 17:40:44 -0800

    Refactored code to picked attention words

--- src/WindowsClient/Controls/NotificationWidget.xaml.cs
+++ src/WindowsClient/Controls/NotificationWidget.xaml.cs
@@ -23,7 +23,6 @@ namespace Talisman
     // --------------------------------------------------------------------------
     public partial class NotificationWidget : Window
     {
-        static Random Rand = new Random();
         TimerInstance _data;
         AppModel _appModel;
 
@@ -31,59 +30,6 @@ namespace Talisman
         public Point Center { get; internal set; }
 
         DraggingLogic _draggingLogic;
-
-        string[] SomeWords = {
-            "scientific",
-            "cellar",
-            "suffer",
-            "return",
-            "structure",
-            "flight",
-            "food",
-            "majestic",
-            "rest",
-            "hall",
-            "overconfident",
-            "experience",
-            "plough",
-            "shy",
-            "include",
-            "satisfying",
-            "blink",
-            "poison",
-            "jumbled",
-            "learn",
-            "bit",
-            "grubby",
-            "spicy",
-            "hunt",
-            "boy",
-            "weak",
-            "twig",
-            "drain",
-            "jam",
-            "fearless",
-            "downtown",
-            "doubtful",
-            "sad",
-            "decision",
-            "hysterical",
-            "follow",
-            "right",
-            "miniature",
-            "humor",
-            "pot",
-            "wire",
-            "horses",
-            "probable",
-            "alleged",
-            "door",
-            "obeisant",
-            "long",
-            "bent",
-            "trace",
-            "stormy" };
-        string[] IgnoreThese = { "min", "outlook", "bluejeans", "http", "https", "com" };
 
         // --------------------------------------------------------------------------
         /// <summary>
@@ -100,37 +46,6 @@ namespace Talisman
 
             this.Loaded += (a, b) =>
             {
-                var lowerText = _data.DecoratedDescription.ToLower();
-                var words = Regex.Split(lowerText, @"[ 0123456789.,/\-?!@#$%^&*()\[\]="":|{ }<> +_]+")
-                    .Where(w => w.Length > 2 && !IgnoreThese.Contains(w)).ToList();
-                while (words.Count < 3)
-                {
-                    var anotherWord = SomeWords[Rand.Next(SomeWords.Length)];
-                    words.Add(anotherWord);
-                    this._data.DescriptionDecoration += $" {anotherWord}";
-                }
-                this._data.NotifyAllPropertiesChanged();
-                lowerText = _data.DecoratedDescription.ToLower();
-
-                string randomWord() => words[Rand.Next(words.Count)];
-
-                var Word1 = randomWord();
-                var Word2 = randomWord();
-                var Word3 = randomWord();
-                var nonWordIndex = Rand.Next(SomeWords.Length);
-                while(lowerText.Contains(SomeWords[nonWordIndex]))
-                {
-                    nonWordIndex = Rand.Next(SomeWords.Length);
-                }
-                switch(Rand.Next(3))
-                {
-                    case 0: Word1 = SomeWords[nonWordIndex]; break;
-                    case 1: Word2 = SomeWords[nonWordIndex]; break;
-                    case 2: Word3 = SomeWords[nonWordIndex]; break;
-                }
-
-                var pickedWords = new { Word1, Word2, Word3};
-                DismissButtons.DataContext = pickedWords;
                 _draggingLogic = new DraggingLogic(this, this);
                 _draggingLogic.OnPositionChanged += (xm, ym) =>
                 {
--- src/WindowsClient/Models/TimerInstance.cs
+++ src/WindowsClient/Models/TimerInstance.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -59,6 +60,29 @@ namespace Talisman
         static int _idCounter = 0;
         public Action OnDeleted = ()=> { };
 
+        string[] SomeWords = {
+            "scientific","cellar","suffer","return",
+            "structure","flight","food","majestic","rest","hall","overconfident","experience","plough","shy","include",
+            "satisfying","blink","poison","jumbled","learn","bit","grubby","spicy","hunt","boy","weak","twig","drain",
+            "jam","fearless","downtown","doubtful","sad","decision","hysterical","follow","right","miniature",
+            "humor","pot","wire","horses","probable","alleged","door","obeisant","long","bent","trace","stormy",
+            "didactic","whip","cheerful","flock","weight","capable",
+            "plastic","company","nasty","injure","fall","lying","defiant","unpack","adorable",
+            "abandoned","plantation","wobble","elegant","complete","team","mellow","acrid","massive",
+            "mailbox","fragile","parallel","used","futuristic","equal","wash","overconfident","finger",
+            "last","wealth","abiding","hook","credit","rely","incompetent","twig","helpless",
+            "shivering","gather","makeshift","anxious","overflow","exist","trees","violent","box","base",
+            "important","eyes","elated","boy","gentle","juggle","long","male","hydrant","meaty",
+            "lamentable","toy","helpful","lackadaisical","float","flag","sparkle","allow","eggnog",
+            "history","furniture","hellish","lunchroom","teeny","hard-to-find","spotted","nondescript","act",
+            "challenge","size","agreeable","pizzas","cough","shelter","modern","trucks","month","three",
+            "miniature","hushed","late","bulb","little","letters","crawl","wholesale","snails","tray",
+        };
+        string[] IgnoreThese = { "min", "outlook", "bluejeans", "http", "https", "com" };
+
+        public object AttentionWords { get; set; }
+        static Random Rand = new Random();
+
         // --------------------------------------------------------------------------
         /// <summary>
         /// ctor
@@ -75,6 +99,65 @@ namespace Talisman
             {
                 links.ToList().ForEach(i => Links.Add(i));
             }
+
+            SetAttentionWords();
+        }
+
+        // --------------------------------------------------------------------------
+        /// <summary>
+        /// Attention words allow us to create attention buttons to the user
+        /// must read the descript to dismiss the appointment
+        /// </summary>
+        // --------------------------------------------------------------------------
+        private void SetAttentionWords()
+        {
+            // get a list of lowercase words from the description
+            var lowerText = Description.ToLower();
+            var words = Regex
+                .Split(lowerText, @"[ 0123456789.,/\-?!@#$%^&*()\[\]="":|{ }<> +_]+")
+                .Where(w => w.Length > 2 && !IgnoreThese.Contains(w)) // at least 3 letters and not on the ignore list
+                .ToList();
+
+            // Make sure we have at least three words in the decorated description
+            while (words.Count < 3)
+            {
+                var anotherWord = SomeWords[Rand.Next(SomeWords.Length)];
+                words.Add(anotherWord);
+                DescriptionDecoration += $" {anotherWord}";
+            }
+
+            string randomWord()
+            {
+                var index = Rand.Next(words.Count);
+                var pickedWord = words[index];
+                words.RemoveAt(index);
+                return pickedWord;
+            }
+
+            // Pick three words at random from the description
+            var Word1 = randomWord();
+            var Word2 = randomWord();
+            var Word3 = randomWord();
+
+            // Pick a word that is definitely not in our decorated description
+            lowerText = DecoratedDescription.ToLower();
+            var nonWordIndex = Rand.Next(SomeWords.Length);
+            while (lowerText.Contains(SomeWords[nonWordIndex]))
+            {
+                nonWordIndex = Rand.Next(SomeWords.Length);
+            }
+
+            // Assign the non-present word to one of our picked words
+            switch (Rand.Next(3))
+            {
+                case 0: Word1 = SomeWords[nonWordIndex]; break;
+                case 1: Word2 = SomeWords[nonWordIndex]; break;
+                case 2: Word3 = SomeWords[nonWordIndex]; break;
+            }
+
+            AttentionWords = new { Word1, Word2, Word3 };
+
+            NotifyAllPropertiesChanged();
         }
 
         // --------------------------------------------------------------------------
